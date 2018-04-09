fc2c626f4b957479b06e4dded07231c74399e4fb
Harpreet Singh <hshssingh4@gmail.com>
2018-04-09 08:34:32 -0500

    sample data file added along with code to distinguish results vs no results

--- src/App.js
+++ src/App.js
@@ -1,84 +1,108 @@
-import React, { Component } from 'react';
-import { Button, Input, Label } from 'semantic-ui-react'
+import React, {Component} from 'react';
+import {Button, Input, Label} from 'semantic-ui-react'
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      results: undefined
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            results: []
+        };
 
-    this.baseUrl =
-        "https://www.googleapis.com/customsearch/v1?key=AIzaSyBpXIM30QZZ3vaNakuU0oiYwRl9hMBIFps" +
-        "&cx=010701151667155428281:junz-utdbgg&q=";
-    this.searchQuery = "";
+        this.baseUrl =
+            "https://www.googleapis.com/customsearch/v1?key=AIzaSyC2ZX4EB6SH1HpWSt9U_wloNBoCAXMw6PE" +
+            "&cx=008731622318034957631:wtj28fwp1ew&q=";
+        this.searchQuery = "";
 
-    this.updateInputValue = this.updateInputValue.bind(this);
-    this.getResults = this.getResults.bind(this)
-  }
+        this.updateSearchQuery = this.updateSearchQuery.bind(this);
+        this.fetchResults = this.fetchResults.bind(this);
+    }
 
-  // API TOKEN
-  //AIzaSyBpXIM30QZZ3vaNakuU0oiYwRl9hMBIFps
+    // API TOKEN
+    //AIzaSyBpXIM30QZZ3vaNakuU0oiYwRl9hMBIFps (Shivan's)
+    //AIzaSyC2ZX4EB6SH1HpWSt9U_wloNBoCAXMw6PE (Mine)
 
-  getResults() {
-    let self = this;
-    let url = this.baseUrl + this.searchQuery;
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials':true,
-        'Access-Control-Allow-Methods':'POST, GET'
-      }
-    })
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(results) {
-      console.log(results.items);
-      self.setState({
-        results: results.items
-      });
-    });
-  }
 
-  updateInputValue(event){
-    this.searchQuery = event.target.value;
-  }
+    updateSearchQuery(event) {
+        this.searchQuery = event.target.value;
+    }
 
-  render() {
-    if(this.state.results === undefined){
-      return (
-        <div>
-          <input value={this.state.inputValue} onChange={this.updateInputValue}/>
-          <Button className="ui button" role="button" onClick={this.getResults}>submit</Button>
-        </div>
+    fetchResults() {
+        let url = this.baseUrl + this.searchQuery;
 
-      );
-    } else {
+        axios.get(url)
+            .then((response) => response.data.items)
+            .then((responseData) => {
+                if (responseData !== undefined) {
+                    console.log(responseData.length + ' results found');
+
+                    // responseData.forEach(function(response) {
+                    //     console.log("Title: " + response.title);
+                    //     console.log("Snippet: " + response.snippet);
+                    //     console.log("\n");
+                    // });
+
+                    console.log(responseData);
+                    this.setState({
+                        results: responseData
+                    });
+                } else {
+                    console.log('no results found');
+
+                    this.setState({
+                        results: []
+                    });
+                }
+            });
+    }
+
+    render() {
+        if (this.state.results.length === 0) {
+            return this.noResultsFoundTemplate();
+        } else {
+            return this.resultsFoundTemplate();
+        }
+    }
+
+    noResultsFoundTemplate() {
         return (
-          <div>
             <div>
-              <input value={this.state.inputValue} onChange={this.updateInputValue}/>
-              <Button className="ui button" role="button" onClick={this.getResults}>Search</Button>
+                <input value={this.state.inputValue}
+                       onChange={this.updateSearchQuery}/>
+                <Button className="ui button"
+                        role="button"
+                        onClick={this.fetchResults}>Submit</Button>
+                <Label>No Results Found</Label>
             </div>
-            <ul>
-              {
-                this.state.results.map((row) => {
-                  return (
-                    <li key={row.id}>
-                      <div>{row.title}</div>
-                      <div>{row.snippet}</div>
-                    </li>
-                  )
-                })}
-            </ul>
-          </div>
+        );
+    }
+
+    resultsFoundTemplate() {
+        return (
+            <div>
+                <div>
+                    <input value={this.state.inputValue}
+                           onChange={this.updateSearchQuery}/>
+                    <Button className="ui button"
+                            role="button"
+                            onClick={this.fetchResults}>Search</Button>
+                </div>
+                <ul>
+                    {
+                        this.state.results.map((row) => {
+                            return (
+                                <li key={row.id}>
+                                    <div>{row.title}</div>
+                                    <div>{row.snippet}</div>
+                                </li>
+                            )
+                        })}
+                </ul>
+            </div>
         )
     }
-  }
 }
 
 export default App;
