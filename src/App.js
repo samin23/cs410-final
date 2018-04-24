35d7d1955474d26cae3abb804ff31cc829eb45ca
Harpreet Singh <hshssingh4@gmail.com>
2018-04-24 14:08:54 -0500

    documentation complete.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import {Button, Input, Dropdown, Checkbox} from 'semantic-ui-react'
 import './App.css';
 import axios from 'axios'
 
-// This const array holds the values for the type of student you are, add more here too add more detailed search
-// The value chosen gets appended to the search string when we send the query to googles custom search engine. For example, if one chooses Undergraduate Student from the dropdown, search query will contain "Undergraduate Student" for specific search results
+// This const array holds the values for the type of student you are, add more here
+// too add more detailed search. The value chosen gets appended to the search string
+// when we send the query to google's custom search engine. For example,
+// if one chooses Undergraduate Student
+// from the dropdown, search query will contain "Undergraduate Student"
+// for specific search results.
 const roleOptions = [
     {text: "None", value: ""},
     {text: "Undergraduate Student", value: "Undergraduate Student"},
@@ -16,7 +20,9 @@ const roleOptions = [
 ];
 
 // This array holds the type of file the user wants to search for
-// When the user selects a filetype, we append "type:{filetype}" to the query and then send to googles custom search engine. For instance, if someone chooses "Article", we append "type
+// When the user selects a filetype, we append "type:{filetype}" to the
+// query and then send to googles custom search engine. For instance, if
+// someone chooses "Faculty Profile", we append "type:faculty_profile" to the search query.
 const filetypeOptions = [
     {text: "None", value: ""},
     {text: 'Article', value: 'article'},
@@ -31,25 +37,34 @@ const filetypeOptions = [
     {text: 'Welcome', value: 'welcome'},
 ];
 
+// This is the definition of the class that contains the JavaScript code.
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             results: []
         };
-        //this baseUrl variable holds the intial api call to the custom search engine with the API key and the CX number included
+
+        // This baseUrl variable holds the initial api call to the custom
+        // search engine with the API key and the CX number included.
+        // We obtained these numbers after going to Google Custom Search
+        // on the web and creating our own custom search box there.
         this.baseUrl =
             "https://www.googleapis.com/customsearch/v1?key=AIzaSyC2ZX4EB6SH1HpWSt9U_wloNBoCAXMw6PE" +
             "&cx=008731622318034957631:wtj28fwp1ew&q=";
-        //the following varibles are the ones we use to send the 
-        //query once the user selects opitions we build the query with these variables
+
+        // The following variables are the ones we use to build the
+        // custom search query. Once the user selects options we build the query
+        // with these variables.
         this.searchQuery = "";
         this.roleText = "";
         this.filetypesText = "";
         this.excludedWordsText = "";
         this.pdfResultsOnly = false;
         
-        //thses bind the functions we have defined below in our program
+        // These bind the functions we have defined below in our program.
+        // They are required since in order for the event handler to catch the
+        // event, one needs to bind these methods in react.
         this.updateSearchQuery = this.updateSearchQuery.bind(this);
         this.fetchResults = this.fetchResults.bind(this);
         this.updateRole = this.updateRole.bind(this);
@@ -66,6 +81,9 @@ class App extends Component {
     // 010701151667155428281:junz-utdbgg (Shivan's)
     // 008731622318034957631:wtj28fwp1ew (Harpreet's)
 
+    // This render function basically contains all the HTML code to layout the
+    // webpage. The className are used for CSS styling and the code for CSS
+    // styling can be found in the App.css file.
     render() {
         return (
             <div className="mainContainer">
@@ -135,15 +153,10 @@ class App extends Component {
         )
     }
 
-    // Event functions when the user types into the search box this function updates the state of that 
-    //variable so we can have a proper query when we send it to google's api
-    updateSearchQuery(event) {
-        this.searchQuery = event.target.value;
-    }
-    
-    //this function fetchResults does not take any input but when its called takes the query and appeneds all the additional
-    //options the user selected and makes a GET call to Google and on reterival of the search results updates the state of 
-    //the page with the resutlts shown
+    /** In general this function is called when a user clicks the submit button for search.
+     * It fetches the results after building the search query based on the currently
+     * selected values in the input boxes and the dropdowns.
+     */
     fetchResults() {
         console.log('FETCHING DATA');
         let searchQuery = this.searchQuery;
@@ -154,10 +167,13 @@ class App extends Component {
         let pdfResultsOnly = this.pdfResultsOnly;
         let pdfQuery = "";
 
+        // If filetype is selected, add that to the query
         if (filetypesText.length > 0) {
             filetypesQuery += "type:" + filetypesText;
         }
 
+        // Add excluded words to the query in a loop because
+        // there can be many space separated words to be exluded
         let excludedWords = this.excludedWordsText.split(" ");
         if (this.excludedWordsText.length > 1 && excludedWords.length > 0) {
             excludedWords.forEach(function (excludedWord) {
@@ -165,19 +181,25 @@ class App extends Component {
             });
         }
 
+        // If pdf only results is selected, add "filetype:pdf" to the queryUrl
         if (pdfResultsOnly) {
             pdfQuery = "filetype:pdf";
         }
 
+        // Build the actual query url for the options selcted here finally
         let queryUrl = roleText + " " + searchQuery +
             " " + filetypesQuery + " " + excludedWordsQuery + " " + pdfQuery;
         console.log(queryUrl);
+
+        // Encode the url in proper format.
         let encodedQueryUrl = encodeURI(queryUrl);
 
-
-        // Build the full url here
+        // Build the the fetch url here by adding base url to custom url.
         let url = this.baseUrl + encodedQueryUrl;
 
+        // This is where the GET called is made to fetch the items.
+        // Once they are fetched, we update the state which calls the
+        // render function to display them on the site.
         axios.get(url)
             .then((response) => response.data.items)
             .then((responseData) => {
@@ -196,26 +218,41 @@ class App extends Component {
                 }
             });
     }
-    // Event functions when the user selects their role this function updates the state of that 
-    //variable so we can have a proper query when we send it to google's api
+
+    // This function is called when the user types into the search box.
+    // Once the user enter a search query, this function updates the state of that
+    // variable so we can have a proper query when we send it to google's api
+    updateSearchQuery(event) {
+        this.searchQuery = event.target.value;
+    }
+
+    // This function is called when the user selects their role. It then
+    // updates the roleText variable so we can append it to our query when
+    // we send it to google's api.
     updateRole(e, data) {
         this.roleText = data.value;
         console.log(this.roleText);
     }
-    // Event functions when the user selects what type of file they want to search for
-    //this function updates the state of that variable so we can have a proper query when we send it to google's api
+
+    // This function is called when the user selects a certain filetype. It then
+    // updates the filetypesText variable so we can append it to our query when
+    // we send it to google's api.
     updateFiletypes(e, data) {
         this.filetypesText = data.value;
         console.log(this.filetypesText);
     }
-    // Event functions when the user types into words to exclude box function updates the state of that 
-    //variable so we can have a proper query when we send it to google's api
+
+    // This function is called when the user enters space separated words to exclude. It then
+    // updates the excludedWordsText variable so we can append it to our query when
+    // we send it to google's api.
     updateExcludedWords(e, data) {
         this.excludedWordsText = data.value;
         console.log(this.excludedWordsText);
     }
-    // Event functions when the user selects the pdf file box this function updates the state of that 
-    //variable so we can have a proper query when we send it to google's api
+
+    // This function is called when the user selects if they only want pdf results. It then
+    // updates the pdfResultsOnly boolean variable so we can append the right
+    // text to our query when we send it to google's api.
     updatePdfResultsOnly(e, data) {
         this.pdfResultsOnly = data.checked;
         console.log(data.checked);
